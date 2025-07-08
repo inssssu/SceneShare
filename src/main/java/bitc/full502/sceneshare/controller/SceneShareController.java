24a52884ab8a18ccebe776ff0f28e01ee376af68
inssssu <xmrtn210845@gmail.com>
2025-07-08 14:32:02 +0900

    3조 팀 프로젝트 sceneshare

---
+++ src/main/java/bitc/full502/sceneshare/controller/SceneShareController.java
@@ -0,0 +1,13 @@
+package bitc.full502.sceneshare.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SceneShareController {
+
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+}
