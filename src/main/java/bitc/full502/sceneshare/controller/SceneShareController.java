68e34fd07a8dec3515a15ddac0f0be2c11d8259b
jinjinstory <jinjinstory@gmail.com>
2025-07-16 11:54:52 +0900

    회원가입(login/join.html) / 회원정보 수정(login/myUpdate.html) 완료 및 마이페이지(login/myPage.html) 회원정보수정 버튼 추가, 영화정보상세페이지(board/reviewDetail.html) 별점주기 추가 (체크한 별점 점수를 숫자로 보여지는 작업 추가 예정) 외 css, js, img 추가 변경됨

--- src/main/java/bitc/full502/sceneshare/controller/SceneShareController.java
+++ src/main/java/bitc/full502/sceneshare/controller/SceneShareController.java
@@ -1,13 +1,57 @@
 package bitc.full502.sceneshare.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class SceneShareController {
 
-    @GetMapping
-    public String index() {
-        return "index";
+    @GetMapping("/main")
+    public String main() {
+        return "user/main";
     }
+
+    @GetMapping("/login")
+    public String login() {
+        return "user/login/login";
+    }
+
+    @GetMapping("/join")
+    public String join() {
+        return "user/login/join";
+    }
+
+    @GetMapping("/myPage")
+    public String myPage() {
+        return "user/login/myPage";
+    }
+
+    @GetMapping("/myUpdate")
+    public String myUpdate() {
+        return "user/login/myUpdate";
+    }
+
+    @GetMapping("/searchList")
+    public String searchList() {
+        return "user/sub/searchList";
+    }
+
+    @GetMapping("/movieDetail")
+    public String movieDetail() {
+        return "user/sub/movieDetail";
+    }
+
+    @GetMapping("/reviewList")
+    public String reviewList() {
+        return "user/board/reviewList";
+    }
+
+    @GetMapping("/reviewDetail")
+    public String reviewDetail() {
+        return "user/board/reviewDetail";
+    }
+
+
 }
--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,46 +1,127 @@
 $(() => {
 
-    $(document).ready(function () {
+  $(document).ready(function () {
 
-        // movie-list bookmark(보고싶어요) 클릭 시 색상 변경
-        $('.bookmark').click(function () {
-            if ($(this).hasClass('active')) {
-                $(this).removeClass('active')
-            }
-            else {
-                $(this).addClass('active')
-            }
-        });
-
-
-        // input[type=search] 커스텀 삭제버튼
-        const input = document.getElementById('search-movie');
-        const clearBtn = document.getElementById('clearBtn');
-
-        input.addEventListener('input', () => {
-            clearBtn.style.display = input.value ? 'block' : 'none';
-        });
-
-        clearBtn.addEventListener('click', () => {
-            input.value = '';
-            input.focus();
-            clearBtn.style.display = 'none';
-        });
-
-
-        // textarea 글자수 
-        const textarea = document.getElementById('myTextarea');
-        const charCount = document.getElementById('charCount');
-
-        textarea.addEventListener('input', () => {
-            charCount.textContent = textarea.value.length;
-        });
-
-
-        //
-        
-
-
+    // movie-list bookmark(보고싶어요) 클릭 시 색상 변경
+    $('.bookmark').click(function () {
+      if ($(this).hasClass('active')) {
+        $(this).removeClass('active')
+      }
+      else {
+        $(this).addClass('active')
+      }
     });
+
+
+    // input[type=search] 커스텀 삭제버튼
+    const input = document.getElementById('search-movie');
+    const clearBtn = document.getElementById('clearBtn');
+
+    input.addEventListener('input', () => {
+      clearBtn.style.display = input.value ? 'block' : 'none';
+    });
+
+    clearBtn.addEventListener('click', () => {
+      input.value = '';
+      input.focus();
+      clearBtn.style.display = 'none';
+    });
+
+
+    // textarea 글자수 
+    const textarea = document.getElementById('myTextarea');
+    const charCount = document.getElementById('charCount');
+
+    textarea.addEventListener('input', () => {
+      charCount.textContent = textarea.value.length;
+    });
+
+
+
+    // 별점 주기
+    const rateWrap = document.querySelectorAll('.rating'),
+      label = document.querySelectorAll('.rating .rating__label'),
+      input2 = document.querySelectorAll('.rating .rating__input'),
+      labelLength = label.length,
+      opacityHover = '0.6';
+
+    let stars = document.querySelectorAll('.rating .star-icon');
+
+    checkedRate();
+  
+    rateWrap.forEach(wrap => {
+      wrap.addEventListener('mouseenter', () => {
+        stars = wrap.querySelectorAll('.star-icon');
+
+        stars.forEach((starIcon, idx) => {
+          starIcon.addEventListener('mouseenter', () => {
+            initStars();
+            filledRate(idx, labelLength);
+
+            for (let i = 0; i < stars.length; i++) {
+              if (stars[i].classList.contains('filled')) {
+                stars[i].style.opacity = opacityHover;
+              }
+            }
+          });
+
+          starIcon.addEventListener('mouseleave', () => {
+            starIcon.style.opacity = '1';
+            checkedRate();
+          });
+
+          wrap.addEventListener('mouseleave', () => {
+            starIcon.style.opacity = '1';
+          });
+        });
+      });
+    });
+
+    function filledRate(index, length) {
+      if (index <= length) {
+        for (let i = 0; i <= index; i++) {
+          stars[i].classList.add('filled');
+        }
+      }
+    }
+
+    function checkedRate() {
+      let checkedRadio = document.querySelectorAll('.rating input[type="radio"]:checked');
+
+
+      initStars();
+      checkedRadio.forEach(radio => {
+        let previousSiblings = prevAll(radio);
+
+        for (let i = 0; i < previousSiblings.length; i++) {
+          previousSiblings[i].querySelector('.star-icon').classList.add('filled');
+        }
+
+        radio.nextElementSibling.classList.add('filled');
+
+
+        function prevAll() {
+          let radioSiblings = [],
+            prevSibling = radio.parentElement.previousElementSibling;
+
+          while (prevSibling) {
+            radioSiblings.push(prevSibling);
+            prevSibling = prevSibling.previousElementSibling;
+          }
+          return radioSiblings;
+        }
+      });
+
+    }
+
+    function initStars() {
+      for (let i = 0; i < stars.length; i++) {
+        stars[i].classList.remove('filled');
+      }
+    }
+
+
+
+  });
 
 });
